cf84178f605298e07d2ab6374460ce44bc9e8ea4
nicodegroeve <nicodegroeve@gmail.com>
2024-12-17 08:43:01 +0000

    heart study 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ data.hist(figsize=(20,20))
 """More specifically, for our research questions:"""
 
 
-selected_variable1 = st.selectbox('Which variable do you want to visualize:', ['LDLC', 'HDLC','TOTCHOL'])
+selected_variable1 = st.selectbox('Which variable do you want to visualize:', ['LDLC', 'HDLC','TOTCHOL', 'CVD'])
 st.write(f'### {selected_variable1} levels')
 fig,ax = plt.subplots (figsize= (10,8))
 ax.hist(x=data[selected_variable1])
@@ -46,15 +46,26 @@ There are a lot of missing values in variables: TOTCHOL, CIGPDAY, BMI, BPMEDS, H
 Since we want to examine HDLC, LDLC and TOTCHOL, we need to handle these missing values before proceeding with the analysis.
 """
 
-data.loc[:, ['LDLC', 'HDLC', 'TOTCHOL']]
+data.loc[:, ['LDLC', 'HDLC']]
 
-data_CHOL = data.dropna(subset = ['LDLC', 'HDLC', 'TOTCHOL'] )
+data_CHOL = data.dropna(subset = ['LDLC', 'HDLC'] )
 data_CHOL.describe()
 
-"""In this table you can see that there are quite a few LDLC and HDLC missing values (8600), this is due to the fact that the values were only measured in period 3. (Explain periods) 
+from sklearn.impute import KNNImputer
+TOTCHOL_data = data[['TOTCHOL']]
+knn_imputer = KNNImputer(n_neighbors = 5)
+TOTCHOL_imputed = knn_imputer.fit_transform(TOTCHOL_data)
+data.loc[:, 'TOTCHOL'] = TOTCHOL_imputed
+
+"""In this table you can see that there are quite a few LDLC and HDLC missing values (8600), this is due to the fact that the values were only measured in period 3. (3 different examination cycles) 
 
 The TOTCHOL outliers were only 409 and were not related to any periods, thus we decided to impute using the KNN imputer (5 neighbors)
 """
+
+data.PERIOD.value_counts()
+
+show_missing_values = ['LDLC','HDLC','TOTCHOL'] 
+data_CHOL[show_missing_values].isnull().sum()
 
 """# Outliers"""
 fig, ax = plt.subplots()
@@ -69,7 +80,7 @@ st.pyplot(fig)
 fig, ax = plt.subplots()
 sns.boxplot(data=data, orient='v', x='CVD', y='TOTCHOL', ax = ax)
 st.pyplot(fig)
-"Finally, there are also some outliers in the TOTCHOL group, ..."
+"Finally, there are also some outliers in the TOTCHOL group, here similar to LDLC the levels can increase to very high levels in certain disorders. We did not want to exclude these people"
 
 """There are some outliers in variables: LDLC and HDLC. In order to continue with our data analysis, we need to correct these."""
 
@@ -94,6 +105,11 @@ st.write(f'### Correlation between {selected_variable} and CVD')
 fig,axes = plt.subplots (figsize= (10,8))
 sns.regplot(x=final_dataset[selected_variable], y= final_dataset['CVD']);
 st.pyplot(fig)
+
+"""although not very prominent the analysis has shown that there is a small, positive association between LDL-C levels and a CVD diagnosis.
+On the other hand the analysis has also shown a slightly larger, negative correlation between HDL-C levels and a CVD diagnosis.
+Finally, for the total cholesterol there is a very slight positive correlation.
+"""
 
 
 """# Models"""
